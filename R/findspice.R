02f5cb0f2a0690872f6cf54449893c4c70da2c39
Qianqian Shan <qianqianshan.am@gmail.com>
2018-10-11 02:12:25 -0500

    Update the windows part on findspice based on the testing results of a win64 system

--- R/findspice.R
+++ R/findspice.R
@@ -7,61 +7,63 @@
 #' @export
 
 findSpice <- function(dylibpath, dylibname) {
-    if (.Platform$OS.type == "unix") {
-        # temp.name <- paste('temp.name',
-        # as.character(Sys.Date()), sep = '')
-        if (is.null(dylibpath)) {
-            # search across all directories to look for
-            # the shared object
-            file.paths <- system(paste0("find ~/ -name ", 
-                dylibname), intern = TRUE)
-            
-        } else {
-            if (dir.exists(dylibpath)) {
-                file.paths <- system(paste0("find ", 
-                  sub("/$", "", dylibpath), 
-                  .Platform$file.sep, " -name ", 
-                  dylibname), intern = TRUE)
-            } else {
-                stop("Dylibpath doesn't exist. Please enter a valid dylibpath.")
-            }
-        }
-        # file.paths <- readLines(con =
-        # 'temp.name')
-        cat(paste("\n Shared library exists in the following directories, the first directory
-          is used. \n"))
-        print(file.paths)
-        if (length(file.paths) == 0) {
-            stop("Shared library of Ngspice not found.  ")
-        } else {
-            return(dirname(file.paths[1]))
-        }
+  if (.Platform$OS.type == "unix") {
+    # temp.name <- paste('temp.name',
+    # as.character(Sys.Date()), sep = '')
+    if (is.null(dylibpath)) {
+      # search across all directories to look for
+      # the shared object
+      file.paths <- system(paste0("find ~/ -name ", 
+                                  dylibname), intern = TRUE)
+      
     } else {
-        # windows, find the shared object dll file
-        if (is.null(dylibpath)) {
-            file.paths <- system(paste0("cmd.exe /c dir /b/s", 
-                dylibname), intern = TRUE)
-        } else {
-            if (dir.exists(dylibpath)) {
-                file.paths <- system(paste0("find ", 
-                  sub("/$", "", dylibpath), 
-                  .Platform$file.sep, " -name ", 
-                  dylibname), intern = TRUE)
-            } else {
-                stop("Dylibpath doesn't exist. Please enter a valid dylibpath.")
-            }
-            # file.paths <- system(paste0('cmd.exe /c
-            # dir /b/s ', sub('/$', '' ,dylibpath) ,
-            # .Platform$file.sep,dylibname), intern =
-            # TRUE)
-            cat(paste("\n Shared library exists in the following directories, the first directory
-          is used. \n"))
-            print(file.paths)
-        }
-        if (length(file.paths) == 0) {
-            stop("Shared library of Ngspice not found. ")
-        } else {
-            return(dirname(file.paths[1]))
-        }
+      if (dir.exists(dylibpath)) {
+        file.paths <- system(paste0("find ", 
+                                    sub("/$", "", dylibpath), 
+                                    .Platform$file.sep, " -name ", 
+                                    dylibname), intern = TRUE)
+      } else {
+        stop("Dylibpath doesn't exist. Please enter a valid dylibpath.")
+      }
     }
+    # file.paths <- readLines(con =
+    # 'temp.name')
+    cat(paste("\n Shared library exists in the following directories, the first directory
+              is used. \n"))
+    print(file.paths)
+    if (length(file.paths) == 0) {
+      stop("Shared library of Ngspice not found.  ")
+    } else {
+      return(dirname(file.paths[1]))
+    }
+  } else {
+    # windows, find the shared object dll file
+    if (is.null(dylibpath)) {
+      file.paths <- system(paste0("cmd.exe /c dir /b/s ", 
+                                  dylibname), intern = TRUE)
+    } else {
+      if (dir.exists(dylibpath)) {
+        # file.paths <- system(paste0("cmd.exe /c where /r ", dylibpath, " ", dylibname))
+        cmd <- paste0(paste0("cmd.exe /c cd ", dylibpath))
+        system(cmd)
+        
+        file.paths <- system(paste0("cmd.exe /c dir /b/s  ",
+                                    dylibname), intern = TRUE)
+      } else {
+        stop("Dylibpath doesn't exist. Please enter a valid dylibpath.")
+      }
+      # file.paths <- system(paste0('cmd.exe /c
+      # dir /b/s ', sub('/$', '' ,dylibpath) ,
+      # .Platform$file.sep,dylibname), intern =
+      # TRUE)
+      cat(paste("\n Shared library exists in the following directories, the first directory
+                is used. \n"))
+      print(file.paths)
+    }
+    if (length(file.paths) == 0) {
+      stop("Shared library of Ngspice not found. ")
+    } else {
+      return(dirname(file.paths[1]))
+    }
+  }
 }
