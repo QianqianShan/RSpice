09993d1a53a982c5d889ce40d61bee091a2bf8a1
Qianqian Shan <qianqianshan.am@gmail.com>
2018-10-31 16:44:51 -0500

    Add new example

---
+++ R/linearregulator.R
@@ -0,0 +1,6 @@
+#' Netlist with circuit set up of a linear regulator.
+#' 
+#' See the vegnette or Linear Regulator Modified from P. Basso: Switched-Mode Power
+#'  Supplies, 2008, Chapter 1, figure 1-2 for a visualization of the 
+#'  circuit. 
+"linearregulator"
